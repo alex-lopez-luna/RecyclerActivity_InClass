d4ffef44bc169021e1f778f4dacab3104cda288f
alex-lopez-luna <tur54397@temple.edu>
2024-09-24 16:50:59 -0400

    Completed all TODO's

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -1,7 +1,9 @@
 package edu.temple.simplerecyclerview
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +15,16 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //TODO Step 2: Provide a LayoutManager
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
 
         //TODO Step 4: Provide a RecyclerView.Adapter
+        val numbers = listOf("One", "Two", "Three", "Four", "Five")
+        val colors = listOf(Color.YELLOW, Color.BLUE, Color.RED, Color.GREEN, Color.DKGRAY)
+
+        //init the adapter
+        val adapter = NumberDisplayAdapter(numbers, colors)
+
+        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,41 @@
 package edu.temple.simplerecyclerview
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.ViewParent
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter(number: List<String>, color: List<Int>): RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() /* TODO Step 3a: Provide Adapter Parent */ {
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+    private val numbers = number
+    private val colors = color
+
+
+    class NumberViewHolder(_textView: TextView) : RecyclerView.ViewHolder(_textView) {
+        val textView = _textView
+    }
 
     // TODO Step 3b: Complete function definitions for adapter
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder(
+            TextView(parent.context).apply {
+                layoutParams = ViewGroup.LayoutParams(300, 300)
+            }
+        )
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        holder.textView.text = numbers[position]
+        holder.textView.setTextColor(colors[position])
+
+    }
 }
